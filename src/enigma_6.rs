bdd59e2dd0e5f74532fea303083dbcd0c3fc7742
Marcus Yearwood <myearwood@users.noreply.github.com>
2018-10-23 18:12:26 -0400

    build out the random number generator. we are gonna need bigger intsgit add . getting overflows :((( also think about moving dedup out of the checkers.

--- src/enigma_6.rs
+++ src/enigma_6.rs
@@ -1,5 +1,27 @@
+use rand::{self, Rng};
 
-fn generate_square(g1: Vec<i32>, g2: Vec<i32>) -> Vec<i32> {
+pub fn get_random_group(min: i32, max: i32) -> (Vec<i32>, Vec<i32>) {
+
+    // thread_rng is often the most convenient source of randomness:
+    let mut rng = rand::thread_rng();
+
+    let mut g1: Vec<i32> = Vec::new();
+    let mut g2: Vec<i32> = Vec::new();
+
+    while g1.len() < 5 {
+        let num: i32 = rng.gen_range(min, max);
+        g1.push(num);
+    }
+
+    while g2.len() < 5 {
+        let num: i32 = rng.gen_range(min, max);
+        g2.push(num);
+    }
+
+    (g1, g2)
+}
+
+pub fn gen_sq(g1: Vec<i32>, g2: Vec<i32>) -> Vec<i32> {
     let mut v: Vec<i32> = Vec::new();
 
     // Row 1
@@ -42,7 +64,6 @@ fn generate_square(g1: Vec<i32>, g2: Vec<i32>) -> Vec<i32> {
 }
 
 
-
 //
 // Unit Tests
 // 
@@ -57,10 +78,17 @@ mod tests {
         let g1 = vec![0, 76, 30, -23, -30];
         let g2 = vec![95, 100, 78, 114, 80];
 
-        let gen_sq = generate_square(g1, g2);
+        let gen_sq = gen_sq(g1, g2);
 
         let expected_result = vec![95, 176, 108, 91, 50, 144, 57, 65, 100, 154, 70, 78, 190, 110, 72, 156, 125, 77, 48, 114, 55, 84, 80, 171, 130];
         assert_eq!(gen_sq, expected_result);
+    }
+
+    #[test]
+    fn test_get_random_group() {
+        let (g1, g2) = get_random_group(0, 200);
+        assert_eq!(g1.len(), 5);
+        assert_eq!(g2.len(), 5);
     }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-mod checker;
-mod etl;
+// mod checker;
+// mod etl;
 mod acc;
 mod sample;
 mod enigma_6;
+extern crate rand;
 
 fn main() {
-    println!("Hello Cruel World!!")
+    let (g1, g2) = enigma_6::get_random_group(-10,200);
+    let mut rand_sq = enigma_6::gen_sq(g1, g2);
+
+    let sums_correct = sample::add_magic(&mut rand_sq, 5);
+    let products_correct = sample::mult_magic(&mut rand_sq, 5);
+    println!("Square: {:?}", rand_sq);
+    println!("Sums Correct: {:?}", sums_correct);
+    println!("Products Correct: {:?}", products_correct);
+
 }
