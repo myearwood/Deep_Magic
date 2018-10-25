cea6bfaff884e52abbb2b184d85a5087cd7ee0f5
Marcus Yearwood <myearwood@users.noreply.github.com>
2018-10-24 21:25:24 -0400

    write code to benchmark the generator and measure squares generated per minute(single threaded). Next Step: CONCURRENCY git add .!

---
+++ benchmarks.md
@@ -0,0 +1,8 @@
+### Benchmarks for Code
+
+#### Enigma #6 
+
+    - Sample
+        - No concurrency
+                - target/debug - ~500k per minute
+                - target/release - 1.425 million per minute
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-mod checker;
-mod etl;
+// mod checker;
+// mod etl;
 mod acc;
 mod sample;
 mod enigma_6;
 extern crate rand;
+use std::time::Instant;
+use std::time::Duration;
 
-fn main() {
+fn gen_sample() {
     let (g1, g2) = enigma_6::get_random_group(-10,200);
     let mut rand_sq = enigma_6::gen_sq(g1, g2);
 
-    let sums_correct = sample::add_magic(&mut rand_sq, 5);
-    let products_correct = sample::mult_magic(&mut rand_sq, 5);
-    println!("Square: {:?}", rand_sq);
-    println!("Sums Correct: {:?}", sums_correct);
-    println!("Products Correct: {:?}", products_correct);
+    let _sums_correct = sample::add_magic(&mut rand_sq, 5);
+    let _products_correct = sample::mult_magic(&mut rand_sq, 5);
+    // println!("Square: {:?}", rand_sq);
+    // println!("Sums Correct: {:?}", sums_correct);
+    // println!("Products Correct: {:?}", products_correct);   
+}
+
+fn main() {
+    let one_min = Duration::new(60, 0);
+    let start = Instant::now();
+    let mut elapsed = start.elapsed();
+    let mut count = 0;
+
+    while elapsed < one_min {
+        gen_sample();
+        count += 1;
+        elapsed = start.elapsed();
+    }
+
+    println!("{} squares generated per minute.", count);
 
 }
